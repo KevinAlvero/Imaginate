42f392e6446f10f2eb4adbe67fb00564f5eba9e9
Kevin Alvero <alverosurono@gmail.com>
2024-10-24 10:03:20 +0700

    second commit

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,7 +15,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
   
-        const response = await fetch('http://localhost:8080/api/v1/dalle', { 
+        const response = await fetch('https://imaginate-wa7y.onrender.com/api/v1/dalle', { 
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
